167a5efa0c6f6d1e2ad2416e1a653426a979d35d
신승훈 <zlzlzlmo@daum.net>
2022-06-07 07:17:03 +0900

    dev 서버 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
