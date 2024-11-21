f4c0c89026945d6b78dc944127ed9e6989d510e9
seunghoon <zlzlzlmo@daum.net>
2024-11-21 15:33:46 +0900

    dotenv설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotionModule } from './notion/notion.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NotionModule],
+  imports: [
+    NotionModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // 환경 변수를 전역으로 사용
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
